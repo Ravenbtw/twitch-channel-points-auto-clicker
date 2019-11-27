70d8d889284826edf385127174fef37287de58fe
Ravenbtw <ingipoulsenfo@gmail.com>
2019-11-27 17:08:03 +0000

    Initial commit.

--- content.js
+++ content.js
@@ -2,7 +2,5 @@ setInterval(() => {
   if (document.querySelector('.claimable-bonus__icon')) {
     console.log('Found bonus button and pressed it.');
     document.querySelector('.claimable-bonus__icon').click();
-  } else {
-    console.log('No bonus button yet.');
   }
-}, 1000*5)
+}, 1000*3)
